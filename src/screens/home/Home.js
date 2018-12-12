358efc89aa12b8833acdd088ee1c429e5d215d53
sugandha <sugs.sehgal2006@gmail.com>
2018-12-12 15:03:31 +0530

    Display home

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,6 +1,49 @@
 import React, { Component } from 'react';
 import './Home.css';
 import Header from '../../common/header/Header';
+import { GridListTile, Typography } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import GridList from '@material-ui/core/GridList';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper
+    },
+    gridListMain: {
+        transform: 'translateZ(0)',
+    },
+    card: {
+        maxWidth: 560,
+        margin: 10,
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    title: {
+        fontWeight: 'strong',
+        color: 'red',
+    },
+    actions: {
+        display: 'flex',
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+        marginLeft: 'auto',
+        [theme.breakpoints.up('sm')]: {
+            marginRight: -8,
+        },
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+});
 
 class Home extends Component {
     constructor() {
@@ -30,32 +73,54 @@ class Home extends Component {
         xhr.addEventListener("readystatechange", function () {
             if (this.readyState === 4) {
                 that.setState({
-                    restaurantList : JSON.parse(this.responseText)
-                 
+                    restaurantList : JSON.parse(this.responseText)             
                 });
-                
-                console.log( JSON.parse(this.responseText) ) ;
-              
+                               
+                console.log( JSON.parse(this.responseText) ) ;            
              }
         });
 
         xhr.open("GET", "http://localhost:8080/api/restaurant");
         xhr.send(data);
-
-
-       
-
     }
 
 
     render() {
+        const { classes } = this.props;
         return (
             <div className="home">
                 <Header />
                 HURRAY
-                console.log( "ss" + {this.state.restaurantList});
+                {this.state.restaurantList.map((pic,index) => ( console.log(pic.restaurantName)  ))}
+
+
+                 <div className="main-body-container">
+                    <GridList cellHeight={"auto"} className={classes.gridListMain} cols={3}>
+                        {/**Check implementation of onClick for GridListTile. If we directly write method name then it executes immediately*/}
+                        {this.state.restaurantList.map(restaurant => (
+                            <GridListTile onClick={() => this.showRestaurantDetails(restaurant.id)}>
+                                <Card key={restaurant.id} className="image-post">
+                                    <CardContent>
+                                        <img src={restaurant.photoUrl} alt="RestaurantImage" />
+                                        <Typography>
+                                            <Typography gutterBottom variant="h5" component="h2">
+                                                {restaurant.restaurantName}
+                                            </Typography>
+                                            <p>{restaurant.categories}</p>
+                                            <div>
+                                                <span className="rating">{restaurant.userRating} ({restaurant.numberUsersRated})</span>
+                                                <span>{restaurant.avgPrice * 2} for two</span>
+                                            </div>
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </GridListTile>
+                        ))}
+                    </GridList>
+                </div>
+               
             </div>
         ) }
 
 }
-export default Home;
+export default withStyles(styles)(Home);
