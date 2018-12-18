ff0763031e6caa64daf4890bdf8e6590a9f3be1f
swaroopchirravuri <swaroopchirravuri@gmail.com>
2018-12-18 21:50:40 +0530

    fixed props issue with search

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -462,9 +462,9 @@ class Header extends Component {
     render() {
         const { classes } = this.props;
         const { open } = this.state;
-        if (this.props.showSearch === "false") {
+        /*if (this.props.showSearch === "false") {
             this.setState({ showSearch: this.props.showSearch });
-        }
+        }*/
         return (
             <div>
                 <header >
@@ -475,7 +475,7 @@ class Header extends Component {
                                     <Toolbar>
                                         <img src={logo} className={classes.logo} alt="FoodOrderingApp" />
 
-                                        {this.state.showSearch === "true" && <div className={classes.search}>
+                                        {this.props.showSearch === "true" && <div className={classes.search}>
                                             <div className={classes.searchIcon}>
                                                 <SearchIcon />
                                             </div>
@@ -488,13 +488,13 @@ class Header extends Component {
                                                 }} onChange={this.inputChangeHandler}
                                             />
                                         </div>}
-                                        {this.state.showSearch === "true" && <div className="login-button">
+                                        {this.props.showSearch === "true" && <div className="login-button">
                                             <Button className={classes.prfileicon} variant="contained" size="medium" color="default" onClick={this.openModalHandler}>
                                                 <ProfileIcon className={classes.prfileicon} />
                                                 Login
                                         </Button>
                                         </div>}
-                                        {this.state.showSearch === "false" && <div className="login-button2">
+                                        {this.props.showSearch === "false" && <div className="login-button2">
                                             <Button className={classes.prfileicon} variant="contained" size="medium" color="default" onClick={this.openModalHandler}>
                                                 <ProfileIcon className={classes.prfileicon} />
                                                 Login
@@ -511,7 +511,7 @@ class Header extends Component {
                                     <Toolbar>
                                         <img src={logo} className={classes.logo} alt="FoodOrderingApp" />
                                         <div className={classes.grow} />
-                                        {this.state.showSearch === "true" && <div className={classes.searchLoggedIn}>
+                                        {this.props.showSearch === "true" && <div className={classes.searchLoggedIn}>
                                             <div className={classes.searchIcon}>
                                                 <SearchIcon />
                                             </div>
